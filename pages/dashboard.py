4fa180c934fac9ca6d40927954d08af3c5dad85c
chetan <chetanlondhe1112@gmail.com>
2022-08-06 14:59:17 +0530

    update 0.2 added @st.experimental_singleton()

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,5 +1,5 @@
 import  streamlit as st
-
+st.session_state.get(st.session_state["authentication_status"])
 if st.session_state["authentication_status"]:
     st.title("Dashboard")
     st.markdown(f'# Welcome *{st.session_state["username"]}*')
