ff9e93ce571f509ea3b5fda6a3131ebe721941e7
chetan <chetanlondhe1112@gmail.com>
2022-08-06 15:00:37 +0530

    update 0.2

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,5 +3,6 @@ st.session_state.get(st.session_state["authentication_status"])
 if st.session_state["authentication_status"]:
     st.title("Dashboard")
     st.markdown(f'# Welcome *{st.session_state["username"]}*')
+    st.subheader("hello")
 else:
     st.warning("Please login")
